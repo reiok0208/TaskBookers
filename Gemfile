6a6aa262a284b1c26ecb0511d8897594830ca901
REI OKADA <rei@REInoMacBook-Pro.local>
2020-03-28 23:45:26 +0900

    [Update3]5-9 ルーティング設計

--- Gemfile
+++ Gemfile
@@ -65,3 +65,8 @@ gem 'devise'
 
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,19 @@
 class BooksController < ApplicationController
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to users_path
+  end
+  def show
+  end
+  def index
+  	@books = Book.all
+  end
 
+  private
+  def book_params
+        params.require(:book).permit(:title, :body)
+  end
 end
