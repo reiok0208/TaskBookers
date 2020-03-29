ea35c755bca706bc0dd4a8244d9c9c5d0d50d014
REI OKADA <rei@REInoMacBook-Pro.local>
2020-03-29 18:48:44 +0900

    [Update4]5-9 ルーティング設計2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,26 @@ class UsersController < ApplicationController
   end
 
   def show
+    @book = Book.new
   	@user = User.find(params[:id])
+  	@books = @user.books
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to user_path(@user)
+    end
+  end
+
+  private
+  def user_params
+        params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '753cbaaf670bdc18d956a54d3e4078b52ec9a73dec804b7c93dfb860f71ca980b99d490b12397d1727287392e61b0f44a98113607681b73644e0f85a6f86368b'
